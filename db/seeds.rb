d8a1ef4caef343c7bdbc965d8c24c4118ddc58f7
Willem Booysen <whbooysen@gmail.com>
2019-11-05 19:40:14 +0100

    basic crud done, needs a lot of cosmetic love

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,12 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#  movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#  Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  new_article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph
+  )
+  new_article.save
+end
